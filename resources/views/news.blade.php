7dd8fd87d0cf22d91856ad5c0b2aa914b50a23a2
Tsani Aflah <tsani@mhs.unsyiah.ac.id>
2022-04-30 08:57:50 +0700

    adding news page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,45 +4,128 @@
 @section('konten')
 <!--accordion-->
 
-<body background="./img/News.png" ;>
-    <h1>Recent Post</h1>
-    <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    NEWS
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <body background="./img/News.png" ;>
+        <div class="my-5 container">
+            <h3>Recent Post</h3>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <img src="/img/N1.jpg" width="100" ;> <big>&nbsp 𝐌𝐢𝐧𝐢𝐬𝐭𝐫𝐲 𝐨𝐟 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 𝐈𝐧𝐚𝐮𝐠𝐮𝐫𝐚𝐭𝐞𝐬 𝐔𝐧𝐬𝐲𝐢𝐚𝐡 𝟐𝟎𝟐𝟐 𝐂𝐡𝐚𝐧𝐜𝐞𝐥𝐥𝐨𝐫</big>
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <center><img src="/img/N11.jpg" width="600" ;></center>
+                            <p align="justify">Prof. Dr. Ir. Marwan, IPU officially became Chancellor of Syiah Kuala University for the period 2022 - 2026. This was marked by the inauguration of Prof. Marwan, on Tuesday
+                                (8/3/2022) by the Secretary General of the Ministry of Education, Culture, Research and Technology (Kemdibudristek) Ir. Suharti, MA., Phd on behalf of Mendikbudristek Nadiem Makarim at Graha
+                                Utama 3rd Floor Kemdibudristek, Jakarta. Prof. Marwan was elected as USK Chancellor on January 24, 2022,replacing the previous Chancellor, Prof. Dr. Ir. Samsul Rizal, M.Eng IPU., ASEAN, Eng.
+                                <img src="/img/News1.png" style="float:left" width="300"> In this activity, Prof. Marwan was inaugurated
+                                along with two other officials, namely the Chancellor of the University of Mataram and the Acting Administrator of LLDIKTI Region I. Also witnessed by the Minister of Agrarian Affairs and Spatial
+                                Planning/National Land Agency DR. Sofyan Djalil and the Secretary General of the DPR RI, Ir. Indra Iskandar, M.Sc. represented by the power of attorney, namely the Director of Human Resources at
+                                Diktiristek, Dr Muhammad Sofwan Efendi. Prof. Abubakar expressed his gratitude to the members of the USK Senate who were present and exercised their right to vote with full integrity. running smoothly.
+                                In the end, it was Prof. Marwan who was elected to lead USK for the next four years. Towards the end of next month, Prof. Marwan was officially inaugurated as USK Chancellor, appointing Prof. Dr. Ir Samsul Rizal MEng
+                                whose term of office ended on 27 February 2022. Prof. Marwan is listed as the 11th rector at Unsyiah/USK.
+                                USK's first acting President was Colonel M. Jasin. The Pangdam Iskandar Muda led USK upon direct appointment by President Soekarno on 27 April 1959 and led USK until 1963. Then, Drs Marzuki Nyakman who was then Deputy Governor
+                                of Aceh was appointed as Chair of the USK Presidium for a term of service from 1863-1965. Next, Prof. Dr. A Madjid Ibrahim was elected Chancellor of USK for two terms, 1965-1973. Since then, the term rector has also been used
+                                for the name of the top leadership position in the USK environment.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <h3>This week</h3>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <img src="/img/N2.jpg" width="100" ;> <big>&nbsp 𝐅𝐨𝐫𝐞𝐢𝐠𝐧 𝐒𝐭𝐮𝐝𝐞𝐧𝐭𝐬 𝐢𝐧 𝐀𝐜𝐞𝐡 𝐆𝐞𝐭 𝐁𝐚𝐬𝐢𝐜 𝐅𝐨𝐨𝐝 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐜𝐞</big>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <center><img src="/img/N2.jpg" width="600" ;></center>
+                            <p align="justify">
+                                "This assistance is distributed to relieve students who are experiencing difficulties in meeting daily basic needs due to the outbreak of the COVID-19 outbreak," said Head of the Jamik Unsyiah Mosque Charity House, Heru Fahlevi in ​​Darussalam, Banda Aceh, Wednesday.
+
+                                He explained that the assistance was targeted at 200 students, with the initial stage, donations having been distributed on March 28, 2020 to dormitory residents and international students."We are working with the Unsyiah Dormitory manager.
+                                They coordinate students and cook ready meals three times a day. Then the food is wrapped and delivered to each room. This is our way to maintain large-scale social restrictions," said Heru.He added that the funds collected at this time were donations from the Acehnese people and the Unsyiah academic community.He said that because the funds collected were still small, donations still prioritized students who were in urgency and met the criteria for assistance.
+
+                                Unsyiah Deputy Chancellor for Academic Affairs, Prof. Marwan said the Ummah Solidarity Fund program was carried out to help ease the burden on students who are currently unable to return to their hometowns."Unsyiah feels obliged to ease the burden on students, moreover, not all students have a good economic life," he said.He also invited the Unsyiah academic community and the wider community to be involved in the social action and donations could be channeled through Bank Syariah Mandiri (BSM) account number 7099400409 a.n. Unsyiah Mosque Charity House."No matter how much assistance is given, it will mean a lot to those in need," said Marwan.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <img src="/img/N3.jpg" width="100" ;> <big>&nbsp 𝐔𝐧𝐬𝐲𝐢𝐚𝐡 𝐆𝐞𝐭𝐬 𝐒𝐩𝐨𝐫𝐭𝐬 𝐄𝐪𝐮𝐢𝐩𝐦𝐞𝐧𝐭 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐜𝐞, 𝐑𝐞𝐜𝐭𝐨𝐫: 𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮, 𝐌𝐫. 𝐌𝐞𝐧𝐩𝐨𝐫𝐚 𝐀𝐦𝐚𝐥𝐢</big>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <center><img src="/img/N33.jpg" width="600" ;></center>
+                            <P align="justify">Minister of Youth and Sports of the Republic of Indonesia, Dr. H. Zainudin Amali, SE., M.Si expressed his wish for Aceh to be included in the grand design of the national sport. He conveyed this in a public lecture at Syiah Kuala University (USK) with the theme 'National Sports Development' at the AAC Dayan Dawood Building. (Banda Aceh, 24/2/2022)
+                                <br>
+                                "Aceh must be included in the grand design of national sports, I hope it grows from USK," said Zainuddin Amali.
+                                So far, Aceh has not been included in the grand design of the national sport, because it is considered that there are still many things that must be fulfilled. So far, the Ministry of Youth and Sports has determined the center points of national sports development in several regions.
+                                Zainuddin Amali views that sport is actually important for the nation, in order to make Indonesia and the region proud in the eyes of the world.
+                                <img src="/img/N333.png" style="float:left" width="300">There are only two occasions when the flag and the national anthem are turned on, the president's visit and there are sports achievements. Those are two activities that made us famous," said the Minister of Youth and Sports. Therefore, he invited all parties to be diligent in exercising. Not everything has to be in sports
+                                achievements or going to the gym, just wanting to move also counts as sports. However, a bright mind cannot be separated from a healthy body. The Minister of Youth and Sports also invites all parties to change the mindset, seeing that sport is not a fad or a side activity. But tree. So far, he says, no parent has ever asked, are you an athlete? On average, when their child is
+                                proposed, what is the job.
+                            </P>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <h3>This Month</h3>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingFour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            <img src="/img/N4.png" width="100" ;> <big>&nbsp 𝐓𝐫𝐚𝐜𝐢𝐧𝐠 𝐑𝐞𝐯𝐞𝐚𝐥𝐬 𝟑𝟎𝟎𝐬 𝐨𝐟 𝐒𝐲𝐢𝐚𝐡 𝐊𝐮𝐚𝐥𝐚 𝐔𝐧𝐢𝐯𝐞𝐫𝐬𝐢𝐭𝐲 𝐒𝐭𝐮𝐝𝐞𝐧𝐭𝐬 𝐏𝐨𝐬𝐢𝐭𝐢𝐯𝐞 𝐟𝐨𝐫 𝐂𝐨𝐯𝐢𝐝-𝟏𝟗</big>
+                        </button>
+                    </h2>
+                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <center><img src="/img/N4.png" width="600" ;></center>
+                            <p align="justify">
+                                "This positive case includes students from 12 faculties. In addition, several lecturers and education staff were also found to have been exposed to Covid-19 in the last two weeks," said USK Banda Aceh Public Relations Coordinator, Ferizal Hasan. Banda Aceh has decided to extend the online lecture period again until March 5, 2022. Previously, based on the development of Covid-19 
+                                cases in the campus environment, USK had implemented online lecture activities until February 20, 2022.
+                                <center><img src="/img/N44.pNg" width="400" ;></center>
+                                Lecturers with comorbid diseases were asked not to be assigned to offline classes. Then, practicum/skill lab/laboratory research/similar activities can be carried out offline in a capacity of only 50 percent. "All student activities outside the campus are also not allowed, if they are carried out online," said Ferizal. Not only that, said Ferizal, in order to increase immunity 
+                                and minimize the impact of exposure to Covid-19, the entire academic community, education staff and Students are required to carry out
+                                a booster dose of vaccination. Evidence of booster vaccine injections must be uploaded to the USK staffing information system for lecturers and education staff.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
-    </div>
+    </body>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
 </body>
+
+</html>
 @endsection
