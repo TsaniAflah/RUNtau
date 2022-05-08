bf47c7b1863b00cb9a61ae7f29dd91fc78c14a6d
Hadafee Mudo <hadafee@mhs.unsyiah.ac.id>
2022-05-08 23:32:36 +0700

    done login and signup

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,14 +7,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap CSS -->
+
+  <link rel="stylesheet" href="/css/template.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <title>RUNtau Official!</title>
   <title>@yield('title')</title>
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 <!-- navigation bar -->
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3FA796; align-items:center " ;>
+  <nav class="navbar navbar-expand-lg navbar-dark shadow" style="background-color: #3FA796; align-items:center " ;>
     <div class=" container-fluid">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -44,6 +49,36 @@
               <a class="nav-link active" href="#"> ABOUT</a>
             </b>
           </li>
+        </ul>
+        <ul class="navbar-nav ms-auto ">
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {{auth()->user()->firstname}} {{auth()->user()->lastname}}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/home"><i class="bi bi-house-door"></i>Home</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-left"></i>Logout</button>
+                </form>
+              </li>
+            </ul>
+          </li>
+
+          @else
+          <li class="nav-item">
+            <a href="/register" class="nav-link active"></i>SignUp</a>
+          </li>
+          <li class="nav-item">
+            <a href="/login" class="nav-link active"></i>Login</a>
+          </li>
+          @endauth
+
         </ul>
       </div>
     </div>
